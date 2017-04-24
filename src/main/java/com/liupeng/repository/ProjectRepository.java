9569e9cbf1b87a8681725744587c4d51b9f9bd6f
hedingjie <1443403247@qq.com>
2017-04-24 23:12:53 +0800

    修复了bug

--- src/main/java/com/liupeng/repository/ProjectRepository.java
+++ src/main/java/com/liupeng/repository/ProjectRepository.java
@@ -1,10 +1,13 @@
 package com.liupeng.repository;
 
 import com.liupeng.model.ProjectEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Date;
 
@@ -14,8 +17,11 @@ import java.sql.Date;
  */
 @Repository
 public interface ProjectRepository extends JpaRepository<ProjectEntity, Integer>{
-    @Query("update ProjectEntity project set project.teamNo=:qTeamNo,project.title=:qTitle," +
-            "project.startDate=:qStartDate,project.endDate=:qEndDate,project.description=:qDescription where project.projectNo=:qProjectNo")
-    void updateProjectByProjectNo(@Param("qTeamNo") String teamNo, @Param("qTitle") String title, @Param("qstartDate")Date startDate,
-    @Param("qEndDate") Date endDate,@Param("qDescription") String description,@Param("qProjectNo") String projectNo);
+    // 修改博文操作
+//    @Modifying
+//    @Transactional
+//    @Query("update ProjectEntity project set project.teamNo=:qTeamNo,project.title=:qTitle," +
+//            "project.startDate=:qStartDate,project.endDate=:qEndDate,project.description=:qDescription where project.projectNo=:qProjectNo")
+//    void updateProjectByProjectNo(@Param("qTeamNo") String teamNo, @Param("qTitle") String title, @Param("qstartDate")Date startDate,
+//    @Param("qEndDate") Date endDate,@Param("qDescription") String description,@Param("qProjectNo") String projectNo);
 }
